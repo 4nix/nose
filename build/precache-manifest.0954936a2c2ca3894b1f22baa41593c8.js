4099d3fda49287f1a2715112ba095bfa1094dbae
wuxutao <wuxutao@mafengwo.cn>
2020-03-25 16:54:19 +0800

    build

---
+++ build/precache-manifest.0954936a2c2ca3894b1f22baa41593c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1a24597e6939161585ffb7999cecb5",
+    "url": "https://nose.wall-e.club/index.html"
+  },
+  {
+    "revision": "5305f6961600b65e28b9",
+    "url": "https://nose.wall-e.club/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "042dcf69a8071c2005d9",
+    "url": "https://nose.wall-e.club/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "5305f6961600b65e28b9",
+    "url": "https://nose.wall-e.club/static/js/2.d63dc8dd.chunk.js"
+  },
+  {
+    "revision": "042dcf69a8071c2005d9",
+    "url": "https://nose.wall-e.club/static/js/main.c135d4c0.chunk.js"
+  },
+  {
+    "revision": "03d37fbb964f458a2244",
+    "url": "https://nose.wall-e.club/static/js/runtime~main.a2b01fbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://nose.wall-e.club/static/media/logo.5d5d9eef.svg"
+  }
+]);
